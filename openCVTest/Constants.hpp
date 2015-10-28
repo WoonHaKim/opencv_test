802124968637824636f4c792bc30943aefcc6b4d
김운하 <cloudedhazelnut@bnsworksui-mac-mini.local>
2015-10-28 14:44:12 +0900

    추출 1차 완료, 실제 코드에 적용필요.

--- openCVTest/Constants.hpp
+++ openCVTest/Constants.hpp
@@ -10,7 +10,8 @@
 #define Constants_h
 
 #define IS_IPHONE [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone
+#define IS_IPAD [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad
 
-
+#define CORNER_PIXEL_THRESHOLD 10
 
 #endif /* Constants_h */
