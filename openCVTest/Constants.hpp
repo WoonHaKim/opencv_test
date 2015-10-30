430c5681f3072bb760407aa6dc60103d04b99dac
김운하 <cloudedhazelnut@bnsworksui-mac-mini.local>
2015-10-30 17:56:55 +0900

    외곽선 추출, 사각형 인식은 미진, 개선 필요

--- openCVTest/Constants.hpp
+++ openCVTest/Constants.hpp
@@ -12,12 +12,34 @@
 #define IS_IPHONE [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone
 #define IS_IPAD [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad
 
-#define CORNER_PIXEL_THRESHOLD 10
 
+
+//카메라 작동 FPS
 #define DEFAULT_FPS 15
 
+
+//영상 처리 알고리즘 설정
 #define DETECT_LINE_METHOD_CANNY 0
 #define DETECT_LINE_METHOD_GAUSSIAN 1
 #define DETECT_LINE_METHOD_HYBRID 2
+#define DETECT_LINE_METHOD_CONTOUR 3
+
+//Coutour 설정
+#define CONTOUR_RECT_RATIO_LIMIT 2
+#define CONTOUR_RECT_AREA_LIMIT 120000
+
+//코너를 몇 픽셀 이내의 선들로 결정할지 지정
+#define CORNER_PIXEL_THRESHOLD 20
+
+
+#define DETECT_AREA_MIN 3000
+#define CONTOUR_RECT_WIDTH_MIN 200
+#define CONTOUR_RECT_HEIGHT_MIN 200
+
+//특징점 매칭 알고리즘 설정
+
+#define DETECT_FEATURE_FAST 0
+#define DETECT_FEATURE_BRISK 1
+#define DETECT_FEATURE_ORB 2
 
 #endif /* Constants_h */
